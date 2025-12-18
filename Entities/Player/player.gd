6229c09fb8aaebdfd26173f46f9e00d19d891890
Skibidhism <imallaboutthatgrass@gmail.com>
2025-12-17 17:14:07 -0800

    Inventory Working

--- Entities/Player/player.gd
+++ Entities/Player/player.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 @export var speed = 200
 @export var item_bar: ItemBar  # assign a Resource in Inspector
 @onready var inventory_ui = $ItemBarUI  # the CanvasLayer node
+@onready var anim = $AnimationPlayer
 
 var screen_size: Vector2
 
@@ -12,7 +13,7 @@ func _ready():
 		inventory_ui.item_bar = item_bar
 		inventory_ui.populate_item_bar()  # now the slots show
 
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	var input_vector = Vector2.ZERO
 
 	if Input.is_action_pressed("move_right"):
@@ -25,11 +26,25 @@ func _physics_process(delta: float) -> void:
 		input_vector.y -= 1
 
 	if input_vector.length() > 0:
-		input_vector = input_vector.normalized() * speed
+		input_vector = input_vector.normalized()
+		play_walk_animation(input_vector)
+	else:
+		anim.play("idle")
 
-	velocity = input_vector
+	velocity = input_vector * speed
 	move_and_slide()
 
-	# Clamp inside screen
 	position.x = clamp(position.x, 0, screen_size.x)
 	position.y = clamp(position.y, 0, screen_size.y)
+
+func play_walk_animation(dir: Vector2):
+	if abs(dir.x) > abs(dir.y):
+		if dir.x > 0:
+			anim.play("walk_down")
+		else:
+			anim.play("walk_down")
+	else:
+		if dir.y > 0:
+			anim.play("walk_down")
+		else:
+			anim.play("walk_up")
